92e1fffdeee44c88e7e75346a5a56ae63559c6a7
Eric Johnson <edj.boston@gmail.com>
2014-04-30 15:53:29 -0700

    :lipstick: whitespace

--- test/matchstick.js
+++ test/matchstick.js
@@ -7,26 +7,30 @@ var assert = require('assert'),
 // Test the Redirect constructor
 describe('Matchstick', function() {
 
-    it('should throw an error for an unrecognized mode', function() {
-    	(function(){
+	/* *
+	 * Validation
+	 */
+
+	it('should throw an error for an unrecognized mode', function() {
+		(function(){
 			matchstick('pattern', 'made-up-mode');
 		}).should.throw("[Matchstick] The 'mode' property must be one of strict, static, wildcard, template, colon, regexp");
 	});
 
-    it('should throw an error for a non-string pattern value', function() {
-    	(function(){
+	it('should throw an error for a non-string pattern value', function() {
+		(function(){
 			matchstick([], 'strict');
 		}).should.throw("[Matchstick] The 'pattern' property must be a string");
 	});
 
-    it('should throw an error for an invalid modifier character', function() {
-    	(function(){
+	it('should throw an error for an invalid modifier character', function() {
+		(function(){
 			matchstick('pattern', 'static', 'x');
 		}).should.throw("[Matchstick] Invalid modifier character 'x'");
 	});
 
-    it('should throw an error for a duplicate modifier character', function() {
-    	(function(){
+	it('should throw an error for a duplicate modifier character', function() {
+		(function(){
 			matchstick('pattern', 'static', 'gg');
 		}).should.throw("[Matchstick] Invalid modifier character 'g'");
 	});
