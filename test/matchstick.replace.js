c676c2fe417f3e82e49cbb757e90f807a83e43e3
Eric Johnson <edj.boston@gmail.com>
2014-04-30 21:20:30 -0700

    Test for replace method in colon mode

--- test/matchstick.replace.js
+++ test/matchstick.replace.js
@@ -17,4 +17,14 @@ describe('Matchstick.replace()', function() {
 		assert.equal(ms.replace(obj), '/project/123/task/abc');
 	});
 
+	// Colon
+	it("should return '/project/123/task/abc' given the pattern '/project/:pid/task/:tid' and {pid:'123', tid:'abc'} data", function() {
+		var ms = matchstick('/project/:pid/task/:tid', 'colon');
+		var obj = {
+			pid : '123',
+			tid : 'abc'
+		};
+		assert.equal(ms.replace(obj), '/project/123/task/abc');
+	});
+
 });
